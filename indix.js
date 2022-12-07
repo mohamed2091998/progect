799c809aa9d37e56c1159dfff4d976b19442df73
mohamed2091998 <medo2030401@gmail.com>
2022-12-07 23:04:22 +0300

    first

---
+++ indix.js
@@ -0,0 +1,12 @@
+var zakah = prompt( "لمعرفة نسبة زكاة المال ؟")
+console .log(0.025* zakah + " جنية ")
+
+
+
+var tax = prompt( " لمعرفة فاتورة الضريبة المالية  ؟")
+console .log(0.014* tax + "جنية")
+
+
+var bank = prompt( "   أهلا وسهلا , نقدم للعملاء معرفة فائدة حسابك المصرفي   ؟")
+console.log(0.017* bank + " جنية")
+
